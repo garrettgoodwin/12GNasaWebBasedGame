c3d33ce18910dabe23941be1f2f898abbc6dfbcc
Neha Balamurugan <nbalamur@asu.edu>
2023-10-13 11:04:06 -0700

    Main camera follows satellite object

---
+++ 3d prototype/Assets/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target; // Reference to the object you want the camera to follow
+    public Vector3 offset = new Vector3(0, -5, 5); // Adjust the offset as needed
+    public float smoothSpeed = 5.0f; // Adjust the smoothness of camera movement
+
+    void LateUpdate()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}
