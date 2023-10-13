f08d8366268d08d992e5356d82fce9309cb07ba8
Neha Balamurugan <nbalamur@asu.edu>
2023-10-13 10:55:54 -0700

    Capsule control using keyboard inputs
    Automatic movement in z, and moved in the x and y by user

--- 3d prototype/Assets/movementController.cs
+++ 3d prototype/Assets/movementController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class movementController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float moveSpeed = 5.0f;
+    public float rotationSpeed = 100.0f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        // Capture user input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Debug.Log("horizontal: " + horizontalInput + "\nvertical: " + verticalInput);
+     
+        // Calculate movement and rotation
+        Vector3 movement = new Vector3(verticalInput, 0.5f, horizontalInput) * moveSpeed * Time.deltaTime; //verticalInput, 0.5f
+        transform.Translate(movement);
         
+        //float rotation = Input.GetAxis("Rotate") * rotationSpeed * Time.deltaTime;
+        //transform.Rotate(Vector3.up * rotation);
     }
 }
