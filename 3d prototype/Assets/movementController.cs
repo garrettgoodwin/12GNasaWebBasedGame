497a432cf35f69925fb414116f28cd36cf7de400
Neha Balamurugan <nbalamur@asu.edu>
2023-10-13 14:59:23 -0700

    Collision counter logged

--- 3d prototype/Assets/movementController.cs
+++ 3d prototype/Assets/movementController.cs
@@ -4,21 +4,44 @@ using UnityEngine;
 
 public class movementController : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
-    public float rotationSpeed = 100.0f;
+    public float moveSpeed = 5.0f; //smoothness of movement
+    public float rotationSpeed = 100.0f; //smoothness of rotation
+
+    public int collisionCount = 0; //starting score is zero
+    public GameObject[] objectsToCount; //the collisions with these objects will increase the score
 
     private void Update()
     {
         // Capture user input
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
-        Debug.Log("horizontal: " + horizontalInput + "\nvertical: " + verticalInput);
+        //Debug.Log("horizontal: " + horizontalInput + "\nvertical: " + verticalInput);
      
         // Calculate movement and rotation
         Vector3 movement = new Vector3(verticalInput, 0.5f, horizontalInput) * moveSpeed * Time.deltaTime; //verticalInput, 0.5f
         transform.Translate(movement);
-        
+
         //float rotation = Input.GetAxis("Rotate") * rotationSpeed * Time.deltaTime;
         //transform.Rotate(Vector3.up * rotation);
+
+        /* TO-DOS:
+         * first person and third person switch 
+         */
+    }
+
+    //when object comes in contact with another collider
+    void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log("Collision detected");
+
+        foreach (GameObject obj in objectsToCount)
+        {
+            if (collision.gameObject == obj)
+            {
+                collisionCount++;
+                //Debug.Log("Collisions: " + collisionCount.ToString());
+                break; // Exit the loop after a collision is detected
+            }
+        }
     }
 }
