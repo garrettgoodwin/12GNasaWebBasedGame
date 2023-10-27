7255e9b4614af06c651112beaa4fc737ecb9248f
Julie cutipie <jkye1@asu.edu>
2023-10-27 01:48:43 -0700

    main menu and scene manager

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+    public void StartStoryMode()
+    {
+        SceneManager.LoadScene("StoryMode");  
+    }
+
+    public void StartTutorial()
+    {
+        SceneManager.LoadScene("Tutorial");  
+    }
+
+    public void StartEndlessMode()
+    {
+        SceneManager.LoadScene("EndlessMode");  
+    }
+
+    public void OpenOptions()
+    {
+        SceneManager.LoadScene("Options");  
+    }
+}
